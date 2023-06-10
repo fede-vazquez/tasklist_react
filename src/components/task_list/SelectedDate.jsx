b9e9fea58392106c3cde1358deb447efc45e5360
FedeVazquez <fedenv2009@hotmail.com>
2023-06-09 22:14:15 -0300

    Agrega switch en el componente "SelectedDate" para mostrar cuantos días de diferencia hay entre la fecha seleccionada y el día actual.

--- src/components/task_list/SelectedDate.jsx
+++ src/components/task_list/SelectedDate.jsx
@@ -1,9 +1,43 @@
+import dayjs from "dayjs";
 import React from "react";
 
 function SelectedDate({ dateSelected }) {
   // función para tomar los días entre las fechas.
-  const daysToThatDate = "dentro de x días";
+  const dateNow = dayjs().startOf("day");
+  const dayDiff = dateSelected.startOf("day").diff(dateNow, "day");
   dateSelected = dateSelected.format("DD/MM/YYYY");
+
+  let daysToThatDate;
+
+  if (dayDiff < 0) {
+    switch (dayDiff) {
+      case -1:
+        daysToThatDate = "Ayer";
+        break;
+      case -2:
+        daysToThatDate = "Antes de ayer";
+        break;
+      default:
+        daysToThatDate = `Hace ${-1 * dayDiff} días`;
+        break;
+    }
+  } else {
+    switch (dayDiff) {
+      case 0:
+        daysToThatDate = "Hoy";
+        break;
+      case 1:
+        daysToThatDate = "Mañana";
+        break;
+      case 2:
+        daysToThatDate = "Pasado mañana";
+        break;
+      default:
+        daysToThatDate = `Dentro de ${dayDiff} días`;
+        break;
+    }
+  }
+
   return (
     <div className="ps-3 py-2">
       <p className="text-white">
