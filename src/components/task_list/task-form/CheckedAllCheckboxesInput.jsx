eae72bbb163ba193b741e6f4548b0144472202cd
FedeVazquez <fedenv2009@hotmail.com>
2023-06-23 10:50:06 -0300

    Separa la lógica del input para agregar todos los días en el formulario de creación de tareas. Agrega un if que mira si uno de los checkboxes ya estaba activo a la hora de apretar el botón de 'Todos los días', el cúal hace un return temprano si esto ocurre, para evitar agregar un día duplicado al array que se guarda en el formulario. Agrega un if en el evento onChange de los checkboxes, para que vean si esta o no activo el checkbox de 'Todos los días', si este esta activo, no se ejecutarán las funciones.

---
+++ src/components/task_list/task-form/CheckedAllCheckboxesInput.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+function CheckedAllCheckboxesInput({
+  daysInWeek,
+  handleCheckedBackground,
+  handleMultipleCheckbox,
+}) {
+  function checkedAllCheckbox(e) {
+    daysInWeek.forEach((day, i) => {
+      const inputDay = document.getElementById(`circle_checkbox_${day.name}`);
+
+      // En el caso de que los checked de inputDay y e.target estén activos,
+      // hace un return temprano para que no se ejecute el código siguiente,
+      // para que no agregue nuevamente el mismo día.
+      if (inputDay.checked && e.target.checked) {
+        return;
+      }
+
+      // Si el check está activado, pone todo en true, en caso contrario lo coloca en false.
+      inputDay.checked = e.target.checked ? true : false;
+
+      // Crea un "evento" para que pueda las funciones puedan trabajar bien.
+      const fakeEvent = {
+        target: {
+          checked: inputDay.checked,
+          parentNode: inputDay.parentNode,
+          getAttribute: () => {
+            return day.name;
+          },
+        },
+      };
+
+      // Usa el handleChecked() para agregar o eliminar los días del form.
+      // y el color de fondo.
+      // El setTimeout es para una pequeña animación
+      setTimeout(() => {
+        handleCheckedBackground(fakeEvent);
+        handleMultipleCheckbox(fakeEvent, "weekDaySelected", day.dayId);
+      }, 30 * i);
+    });
+  }
+
+  return (
+    <div className="d-flex align-items-center justify-content-between px-2 form-switch pt-2">
+      <label htmlFor="task_check_repeat_all_days">
+        <p>Todos los días</p>
+      </label>
+      <input
+        id="task_check_repeat_all_days"
+        className="form-check-input m-0"
+        type="checkbox"
+        role="switch"
+        name="repeatAllDay"
+        onChange={(e) => {
+          checkedAllCheckbox(e);
+        }}
+      />
+    </div>
+  );
+}
+
+export default CheckedAllCheckboxesInput;
--- src/components/task_list/task-form/DaysSelector.jsx
+++ src/components/task_list/task-form/DaysSelector.jsx
@@ -1,55 +1,28 @@
-import React, { useRef } from "react";
+import React from "react";
+import CheckedAllCheckboxesInput from "./CheckedAllCheckboxesInput";
+
+const daysInWeek = [
+  { dayId: 1, name: "Lunes" },
+  { dayId: 2, name: "Martes" },
+  { dayId: 3, name: "Miércoles" },
+  { dayId: 4, name: "Jueves" },
+  { dayId: 5, name: "Viernes" },
+  { dayId: 6, name: "Sábado" },
+  { dayId: 7, name: "Domingo" },
+];
+
+function handleCheckedBackground(e) {
+  const { checked } = e.target;
+  const containerInput = e.target.parentNode;
+  if (checked) {
+    containerInput.classList.add("circle-checkbox-active");
+  } else {
+    containerInput.classList.remove("circle-checkbox-active");
+  }
+}
 
 function DaysSelector({ handleMultipleCheckbox }) {
-  const allDaysSelected = useRef();
-
-  const daysInWeek = [
-    { dayId: 1, name: "Lunes" },
-    { dayId: 2, name: "Martes" },
-    { dayId: 3, name: "Miércoles" },
-    { dayId: 4, name: "Jueves" },
-    { dayId: 5, name: "Viernes" },
-    { dayId: 6, name: "Sábado" },
-    { dayId: 7, name: "Domingo" },
-  ];
-
-  function handleCheckedBackground(e) {
-    const { checked } = e.target;
-    const containerInput = e.target.parentNode;
-    if (checked) {
-      containerInput.classList.add("circle-checkbox-active");
-    } else {
-      containerInput.classList.remove("circle-checkbox-active");
-    }
-  }
-
-  function checkedAllCheckbox(e) {
-    daysInWeek.forEach((day, i) => {
-      const inputDay = document.getElementById(`circle_checkbox_${day.name}`);
-
-      // Si el check está activado, pone todo en true, en caso contrario lo coloca en false.
-      inputDay.checked = e.target.checked ? true : false;
-
-      // Crea un "evento" para que pueda las funciones puedan trabajar bien.
-      const fakeEvent = {
-        target: {
-          checked: inputDay.checked,
-          parentNode: inputDay.parentNode,
-          getAttribute: () => {
-            return day.name;
-          },
-        },
-      };
-
-      // Usa el handleChecked() para agregar o eliminar los días del form.
-      // y el color de fondo.
-      // El setTimeout es para una pequeña animación
-      setTimeout(() => {
-        handleCheckedBackground(fakeEvent);
-        handleMultipleCheckbox(fakeEvent, "weekDaySelected", day.dayId);
-      }, 30 * i);
-    });
-  }
+  const allDaysSelected = document.getElementById("task_check_repeat_all_days");
 
   return (
     <div className="my-2">
@@ -68,29 +41,20 @@ function DaysSelector({ handleMultipleCheckbox }) {
               customattribute={day.name}
               type="checkbox"
               onChange={(e) => {
-                handleMultipleCheckbox(e, "weekDaySelected", day.dayId);
-                handleCheckedBackground(e);
+                if (!allDaysSelected?.checked) {
+                  handleMultipleCheckbox(e, "weekDaySelected", day.dayId);
+                  handleCheckedBackground(e);
+                }
               }}
             />
           </li>
         ))}
       </ul>
-      <div className="d-flex align-items-center justify-content-between px-2 form-switch pt-2">
-        <label htmlFor="task_check_repeat_all_days">
-          <p>Todos los días</p>
-        </label>
-        <input
-          ref={allDaysSelected}
-          id="task_check_repeat_all_days"
-          className="form-check-input m-0"
-          type="checkbox"
-          role="switch"
-          name="repeatAllDay"
-          onChange={(e) => {
-            checkedAllCheckbox(e);
-          }}
-        />
-      </div>
+      <CheckedAllCheckboxesInput
+        daysInWeek={daysInWeek}
+        handleCheckedBackground={handleCheckedBackground}
+        handleMultipleCheckbox={handleMultipleCheckbox}
+      />
     </div>
   );
 }
